fda5ce91634624391ce265af8828fa8dc95c4f51
DarkSpear <admin@lawal.org>
2026-08-24 09:24:48 +0100

    Remove unused Firebase bootstrap prototype

--- functions/bootstrap.js
+++
@@ -1,5 +0,0 @@
-const existingFunctions = require('./index');
-const githubOAuth = require('./github-oauth');
-
-module.exports = existingFunctions;
-module.exports.githubOAuthExchange = githubOAuth.githubOAuthExchange;
